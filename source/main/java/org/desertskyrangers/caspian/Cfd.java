3a0ce2244db6a971891948e757cb51af8ff5a1fa
Mark Soderquist <msoderquist@mastercontrol.com>
2022-04-11 13:35:37 -0600

    Programmer notes

--- source/main/java/org/desertskyrangers/caspian/Cfd.java
+++ source/main/java/org/desertskyrangers/caspian/Cfd.java
@@ -4,6 +4,9 @@ public class Cfd {
 
 	public static double calcLift( Air air, Aircraft aircraft, Airfoil airfoil, double speed ) {
 		// 0.5 * kg/m^3 * m/s * m/s * cm^2 * %
+		// kg/m*m*m * m/s * cm*cm
+		// kg*m / s^2
+		// N
 		return 0.5 * air.density() * speed * speed * aircraft.wingarea() * airfoil.liftCoefficient();
 	}
 
